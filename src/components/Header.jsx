2f5df8853dd1b5f69c8ff39324ecdf8a1cd22dd3
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2021-08-08 17:09:27 -0500

    concetando el formulario y llenado a payment

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { FaShoppingBasket } from 'react-icons/fa';
+import AppContext from '../context/AppContext';
+import '../styles/components/Header.css';
+
+const Header = () => {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
+  return(
+    <div className='Header'>
+      
+      <h1 className='Header-title' >
+        <Link to='/'>
+          Platzi Conf Merch
+        </Link>
+      </h1>
+      
+      <div className="Header-checkout">
+        <Link to='/checkout'>
+          <FaShoppingBasket />
+        </Link>
+        {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
+      </div>
+    </div>
+  )
+};
+
+export default Header;
---
+++ src/containers/Success.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/components/Success.css';
+
+const Success = () => (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Cristian, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu dirección</span>
+        <div className="Success-map">
+          Google maps
+        </div>
+      </div>
+    </div>
+  )
+
+export default Success;
