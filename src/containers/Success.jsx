bcc7861cb2acf3bdace96e8a1b7ee69e8d94d547
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2021-08-10 20:29:12 -0500

    sin mapa

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
+import useGoogleAddres from '../hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
-const Success = () => (
+const Success = () => {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
+  const location = useGoogleAddres(buyer[0].address)
+
+  return (
     <div className="Success">
       <div className="Success-content">
-        <h2>Cristian, Gracias por tu compra</h2>
+        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 dias a tu dirección</span>
         <div className="Success-map">
-          Google maps
+          <Map data={location}/>
         </div>
       </div>
     </div>
-  )
+  )}
 
 export default Success;
